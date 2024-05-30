59c2c3f6881b0d0e453bd1240cbf693de8c1caf0
CrazyKittenn <starninjagamer@gmail.com>
2024-05-30 18:46:41 +0200

    CSS Change

--- skripta.php
+++ skripta.php
@@ -8,15 +8,15 @@
     <meta name="author" content="Filip Gredelj">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/zajednici.css">
-    <link rel="stylesheet" href="css/skripta.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/clanak.css">
+    <title>Pregled unosa</title>
 </head>
 
 <body>
     <nav>
         <ul>
             <li>
-                <a href="index.html">MOPO</a>
+                <a href="index.html">Game News</a>
             </li>
             <li><a href="index.html">Home</a></li>
             <li><a href="#" id="nav_active">Unos</a></li>
@@ -28,39 +28,34 @@
         <div id="content-wrap">
             <main>
                 <?php
-                $naslov = "";
-                $kratakS = "";
-                $sadrzaj = "";
-                $kategorija = "";
-                $slikaPath = "";
-                $slikaIme = "";
-                $arhive = false;
-                if (isset($_POST["naslov"])) {
-                    $naslov = $_POST["naslov"];
-                }
-                if (isset($_POST["kratakS"])) {
-                    $kratakS = $_POST["kratakS"];
-                }
-                if (isset($_POST["sadrzaj"])) {
-                    $sadrzaj = $_POST["sadrzaj"];
-                }
-                if (isset($_POST["k"])) {
-                    $kategorija = $_POST["k"];
-                }
-                if (isset($_FILES["slika"])) {
-                    $slikaPath = base64_encode(file_get_contents($_FILES["slika"]["tmp_name"]));
-                    $slikaIme = $_FILES["slika"]["name"];
-                }
+                $naslov = $_POST["naslov"];
+                $kratakS = $_POST["kratakS"];
+                $sadrzaj = $_POST["sadrzaj"];
+                $kategorija = $_POST["k"];
+                $slika = $_FILES["slika"]["name"];
+                $target_dir = "Images/$slika";
+                move_uploaded_file($_FILES["slika"]["tmp_name"], $target_dir);
+                $arhive = 0;
                 if (isset($_POST["check"])) {
-                    $arhive = $_POST["check"];
+                    $arhive = 1;
                 }
-                echo "<h1>$naslov</h1>";
-                echo "<p>$kratakS</p>";
-                echo "<p>$sadrzaj</p>";
-                echo "<p>$kategorija</p>";
-                echo "<img src='$slikaPath' alt='$slikaIme'>";
-                echo "<p>$arhive</p>";
                 ?>
+                <article>
+                    <section>
+                        <h1><?php echo $naslov; ?></h1>
+                        <hr>
+                        <p>Autor</p>
+                        <hr>
+                        <p>Datum</p>
+                        <hr>
+                        <p><?php echo $kategorija; ?></p>
+                    </section>
+                    <?php echo "<img src='Images/$slika' alt='$slika' width='60%'>"; ?>
+                    <section>
+                        <p><?php echo $sadrzaj; ?></p>
+                    </section>
+                </article>
+                <hr>;
             </main>
         </div>
         <footer id="footer">
